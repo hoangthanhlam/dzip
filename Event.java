0d8c8705b4c16234edada9034d973870a5b6420b
lamdot09@gmail.com <lamdot09@gmail.com@631da8a9-069f-349d-53de-8f80c3519707>
2013-10-11 13:48:03 +0000

    7

---
+++ Event.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dzip;
+
+/**
+ * Event class
+ * @author thoang
+ */
+public class Event {
+    int id; // id of the event
+    int timestamp; // timestamp
+    
+    Event(int i, int t){
+        id=i;
+        timestamp=t;
+    }
+}
